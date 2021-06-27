28628624d57945470391c51fea395273680638b1
noname <ysf.kya34@gmail.com>
2021-06-27 16:05:15 +0300

    Initial commit

---
+++ boards/tests/test_view_home.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.urls.base import resolve
+from django.contrib.auth.models import User
+from ..views import BoardListView, home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
+
+class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+    
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func.view_class, BoardListView)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
